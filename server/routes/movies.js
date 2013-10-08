daafca2de85d6251d6447cdd8b58ee1447761d41
shantich <sebastian.hantich@gmail.com>
2013-10-08 14:13:47 +0200

    fixed bug task 10

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -87,9 +87,9 @@ exports = module.exports = function (db) {
         node.data.type = 'movie';
         node.data.id = uuid.v4();
         //task 10
-        if (node.data.title.length == 0) {
+        if (node.data.title.length === 0) {
             return res.status(422).send();
-        };
+        }
 
         logger.debug('Adding a new movie');
         node.save(function (err, savedNode) {
@@ -121,9 +121,9 @@ exports = module.exports = function (db) {
             }
 
             //task 10
-            if (req.body.title.length == 0) {
+            if (req.body.title.length === 0) {
                 return res.status(422).send();
-            };
+            }
 
             node.data.title = req.body.title;
             node.data.description = req.body.description;
